a1fde719c4fb53a9ea3d13d7b10bf031cc6cc514
AthranFernandez <74589575+AthranFernandez@users.noreply.github.com>
2020-11-20 17:32:46 +0800

    Create HelloWorld.java
    My Second Commit

---
+++ Athran Fernandez/HelloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+*
+*
+*@Author Athran Fernandez
+*/
+
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}//end of main class
+}//end of class "HelloWorld"
